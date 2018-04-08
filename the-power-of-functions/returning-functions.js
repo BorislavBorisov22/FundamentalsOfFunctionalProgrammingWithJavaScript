a026bd04bb5d1b0bed4706f2078fe38e09459cfd
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-04-09 00:16:19 +0300

    added currying examples

--- the-power-of-functions/returning-functions.js
+++ the-power-of-functions/returning-functions.js
@@ -1,3 +1,5 @@
+const R = require('rambda');
+
 const users = [{
     id: 1,
     name: 'somename'
@@ -14,3 +16,29 @@ const byId = (id) => {
 
 const targetUser = users.find(byId(2));
 console.log(targetUser);
+
+// using rambdas currying to replicate the above behaviour
+const byIdWithRambda = R.curry((id, item) => {
+    return item.id === id;
+});
+
+const targetUserWithRambda = users.find(byIdWithRambda(2));
+console.log(targetUserWithRambda);
+
+const add = (a) => {
+    return (b) => {
+        return (c) => {
+            return a + b + c;
+        }
+    };
+};
+
+const addWithRambda = R.curry((a, b, c) => {
+    return a + b + c;
+});
+
+const sum = add(1)(2)(3);
+const sumWithRambda = addWithRambda(1)(2)(3);
+
+console.log(sum);
+console.log(sumWithRambda);
